c0f284f65d2e154b0160e33ed6d46084937b9f1d
Travis Lubbers <travisl@digi.com>
2013-05-07 15:43:56 -0500

    Whitespace and comments

--- three.hs
+++ three.hs
@@ -1,24 +1,28 @@
 
 -- Largest prime factor of 600851475143
 
--- factors currentFactor 
+-- factors currentFactor
 -- [2,3] ++ [6*n-1,6*n+1]
 
--- let 
+-- let
 --   testList = [2,3,5,..]
 --   factors = []
 --   in
 --  print factor n factors testList
 
-_factor n factors testList = 
+_factor n factors testList =
   let
     testNum = head testList
   in
    case n `divMod` testNum of
-     (1,0) -> testNum:factors
-     (n', 0) -> _factor n' (testNum:factors) testList
-     _ -> _factor n factors (tail testList)
-          
+     -- Terminate, largest factor     
+     (1,0) -> testNum:factors 
+     -- divides evenly
+     (n', 0) -> _factor n' (testNum:factors) testList 
+     -- Try next
+     _ -> _factor n factors (tail testList) 
+
 factor n =
   _factor n [] (2:[3,5..])
-  
+
+main = print (factor 600851475143)
